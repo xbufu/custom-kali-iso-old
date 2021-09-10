4959d472dc728283ccb5ad77f604a172d964666f
bufu <bufu@custom-kali-iso.bufu-sec.local>
2021-09-10 22:44:20 +0200

    updates

---
+++ .getopt.sh
@@ -0,0 +1,18 @@
+# Supported options are (from build.sh):
+# -d | --distribution <distro>
+# -p | --proposed-updates
+# -a | --arch <architecture>
+# -v | --verbose
+# -D | --debug
+# -s | --salt
+#      --installer
+#      --live
+#      --variant <variant>
+#      --version <version>
+#      --subdir <directory-name>
+#      --get-image-path
+#      --no-clean
+#      --clean
+
+BUILD_OPTS_SHORT="d:pa:vDs"
+BUILD_OPTS_LONG="distribution:,proposed-updates,arch:,verbose,debug,salt,installer,live,variant:,version:,subdir:,get-image-path,no-clean,clean"
